7cbc73c43cb87a41c3e329563dab1248f842b08e
Vyshak NB <60031564+VyshakNB@users.noreply.github.com>
2026-07-22 20:44:47 +0530

    Update theiarun.ps1

--- theia/theiarun.ps1
+++ theia/theiarun.ps1
@@ -3,6 +3,12 @@ Write-Output "Starting Theia server on port 3000..."
 $logFile = "C:\VCollab\logs\Theia.log"
 $errorFile = "C:\VCollab\logs\TheiaError.log"
 
+
+$env:NODE_USE_ENV_PROXY = "1"
+$env:HTTP_PROXY = "http://10.38.107.203:3120"
+$env:HTTPS_PROXY = "http://10.38.107.203:3120"
+$env:NO_PROXY = "localhost,127.0.0.1,::1"
+
 Start-Process -FilePath "npm.cmd" -ArgumentList "run start" -WindowStyle Hidden -RedirectStandardOutput $logFile -RedirectStandardError $errorFile
 
 Set-Location ..
